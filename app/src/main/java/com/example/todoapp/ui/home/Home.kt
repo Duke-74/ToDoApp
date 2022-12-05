d84b64aa32e436b3e8af4671ce4f38079db19da0
Maxim <belyanin-max@mail.ru>
2022-12-05 17:17:49 +0300

    Initial commit

---
+++ app/src/main/java/com/example/todoapp/ui/home/Home.kt
@@ -0,0 +1,8 @@
+package com.example.todoapp.ui.home
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen(){
+
+}
---
+++ app/src/main/java/com/example/todoapp/ui/home/detail/Detail.kt
@@ -0,0 +1,9 @@
+package com.example.todoapp.ui.home.detail
+
+import com.example.todoapp.Graph
+import com.example.todoapp.data.TodoDataSource
+
+//class DetailViewModel(
+//    private val todoDataSource: TodoDataSource = Graph.todoRepo
+//    private val id: Long
+//)
